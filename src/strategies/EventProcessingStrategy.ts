900f01e44b8d6dcc1daeb89c6021199adec56e61
Diogo Corrêa <diogocorrea@id.uff.br>
2025-08-02 04:09:28 -0300

    refactor: update barrel exports for strategies
    - Convert main strategy file to barrel export
    - Maintain backward compatibility
    - Clean import statements across project
    - Follow single responsibility principle

---
+++ src/strategies/EventProcessingStrategy.ts
@@ -0,0 +1,5 @@
+export type { IEventProcessingStrategy } from '../interfaces/IEventProcessingStrategy';
+export { DirectProcessingStrategy } from './DirectProcessingStrategy';
+export { EventProcessingContext } from './EventProcessingContext';
+export { HybridProcessingStrategy } from './HybridProcessingStrategy';
+export { QueueProcessingStrategy } from './QueueProcessingStrategy';
